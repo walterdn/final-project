e8196a0733d1392f6059072f02896606ea146c48
walter <walternicholas@gmail.com>
2015-12-20 16:47:16 -0800

    heroku stuff

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var authRouter = require(__dirname + '/routes/auth_routes');
 
 process.env.APP_SECRET = process.env.APP_SECRET || 'hello';
 
-mongoose.connect(process.env.MONGOLAB_URI || 'mongodb://localhost/song_app');
+mongoose.connect(process.env.MONGOLAB_URI || 'mongodb://heroku_dh1ztl0r:hae5nmcsuh3tteoi3i1r0cd6kn@ds033875.mongolab.com:33875/heroku_dh1ztl0r');
 
 app.use(express.static(__dirname + '/build'));
 
