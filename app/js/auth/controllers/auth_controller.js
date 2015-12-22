3d676d4b2113f483bec8dc1048d93c00fd2f0b64
walter <walternicholas@gmail.com>
2015-12-21 21:02:40 -0800

    prevented duplicate users. fixed bug with loading songs

--- app/js/auth/controllers/auth_controller.js
+++ app/js/auth/controllers/auth_controller.js
@@ -2,7 +2,6 @@ module.exports = function(app) {
   app.controller('AuthController', ['$scope', '$http', '$cookies', '$location', function($scope, $http, $cookies, $location) {
     
     $scope.logOut = function() {
-      // $scope.reset();
       $scope.token = null;
       $scope.currentUser = null;
       $cookies.remove('token');
@@ -21,7 +20,8 @@ module.exports = function(app) {
     };
 
     $scope.songLoader = function(song) { //gets set in save_songs_controller from the /savesongs view, and then called from in client.js from /songs view
-      if(song) $scope.loadThisSong = song;
+      if (song == 'clearSongLoader') $scope.loadThisSong = null;
+      if (song) $scope.loadThisSong = song;
       else return $scope.loadThisSong;
     };
   }]);
